f86234ec4084c5b5d2b6d7469c5555b272582359
Benjamin Saks <saksben2@gmail.com>
2025-08-12 16:08:56 -0700

    site update

---
+++ src/data/navData.ts
@@ -0,0 +1,49 @@
+export interface NavItem {
+    key: string;
+    url: string;
+    children?: NavItem[]
+}
+
+const navData: NavItem[] = [
+  {
+    key: "Home",
+    url: "/",
+    children: [],
+  },
+  {
+    key: "About",
+    url: "/about/",
+    children: [],
+  },
+  {
+    key: "Services",
+    url: "/services/",
+    children: [],
+  },
+  {
+    key: "More",
+    url: "/more/",
+    children: [
+      { 
+        key: "Testimonials", 
+        url: "/more/reviews/" 
+      },
+      {
+        key: "Gallery",
+        url: "/more/gallery/",
+      },
+    ],
+  },
+
+  // {
+  //   key: "Blog",
+  //   url: "/blog/",
+  //   children: [],
+  // },
+  {
+    key: "Contact",
+    url: "/contact/",
+    children: [],
+  },
+];
+export default navData;
