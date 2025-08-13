5c2a21465dff6231bcc5998dbda2e5456c3a178b
Benjamin Saks <saksben2@gmail.com>
2025-08-13 12:39:53 -0700

    organize images and icons

---
+++ src/assets/index.ts
@@ -0,0 +1,121 @@
+// Image imports
+// import Img from "../../assets/images/demo/"
+import Cabinets2 from "../../assets/images/demo/cabinets2.jpg";
+import Construction16 from "../../assets/images/demo/construction16.jpg";
+import Construction17 from "../../assets/images/demo/construction17.jpg";
+import EnvironmentSlogan from "../../assets/images/demo/environment-slogan.png";
+import Hero from "../../assets/images/demo/hero.jpg";
+import ImagesPeoplePerson7 from "../../assets/images/demo/Images_People_person7.jpg";
+import Meeting7 from "../../assets/images/demo/meeting7.jpg";
+import Port1 from "../../assets/images/demo/portfolio/port1.jpg";
+import Port2 from "../../assets/images/demo/portfolio/port2.jpg";
+import Port4 from "../../assets/images/demo/portfolio/port4.jpg";
+import Port5 from "../../assets/images/demo/portfolio/port5.jpg";
+import Port7 from "../../assets/images/demo/portfolio/port7.jpg";
+import Port8 from "../../assets/images/demo/portfolio/port8.jpg";
+import Tax1 from "../../assets/images/demo/tax1.jpg";
+import Profile4 from "../../assets/images/demo/profile-4.png";
+import Profile5 from "../../assets/images/demo/profile5.png";
+
+// Icon imports
+// import Svg from "./icons/"
+import Award from "./icons/award.svg";
+import AwardGrey from "./icons/award-grey.svg";
+import Call from "./icons/call.svg";
+import CheckBlue from "./icons/check-blue.svg";
+import CheckCircle from "./icons/check-circle.svg";
+import CheckGrey from "./icons/check-grey.svg";
+import ClockZoom from "./icons/clock-zoom.svg";
+import DownGold from "./icons/down-gold.svg";
+import EmailRed2 from "./icons/email-red2.svg";
+import Facebook from "./icons/Facebook.svg";
+import Facebook1A from "./icons/facebook-1a.svg";
+import Google from "./icons/google.svg";
+import GroupGrey from "./icons/group-grey.svg";
+import IconsMail from "./icons/Icons_mail.svg";
+import IconsPhone from "./icons/Icons_phone.svg";
+import IconsSun from "./icons/Icons_sun.svg";
+import ImagesGraphicsFlower from "./icons/Images_Graphics_big-abstract-flower.svg";
+import ImagesLargeWaves from "./icons/Images_Graphics_large-waves.svg";
+import ImagesSpiderSense from "./icons/Images_Graphics_spider-sense.svg";
+import Instagram from "./icons/instagram.svg";
+import Instagram1a from "./icons/instagram1a.svg";
+import Like from "./icons/Like.svg";
+import LogoBlack from "./icons/logo-black.svg";
+import LogoLight from "./icons/logo-light.svg";
+import Mechanic1 from "./icons/mechanic1.svg";
+import Mechanic2 from "./icons/mechanic2.svg";
+import MechEmail from "./icons/mech-email.svg";
+import MechPhone from "./icons/mech-phone.svg";
+import MechPin from "./icons/mech-pin.svg";
+import Members from "./icons/members.svg";
+import PersonGrey from "./icons/person-grey.svg";
+import Phone1A from "./icons/phone-1a.svg";
+import PhoneRed2 from "./icons/phone-red2.svg";
+import SheildBlack from "./icons/sheild-black.svg";
+import Speedometer from "./icons/speedometer.svg";
+import StarsYellow from "./icons/stars-yellow.svg";
+import Svg247 from "./icons/247.svg";
+import Twitter1A from "./icons/twitter-1a.svg";
+import Youtube1A from "./icons/youtube1a.svg";
+
+export {
+  // Image exports
+  Cabinets2,
+  Construction16,
+  Construction17,
+  EnvironmentSlogan,
+  Hero,
+  ImagesPeoplePerson7,
+  Meeting7,
+  Port1,
+  Port2,
+  Port4,
+  Port5,
+  Port7,
+  Port8,
+  Tax1,
+  Profile4,
+  Profile5,
+
+  // Icon exports
+  Award,
+  AwardGrey,
+  Call,
+  CheckBlue,
+  CheckCircle,
+  CheckGrey,
+  ClockZoom,
+  DownGold,
+  EmailRed2,
+  Facebook,
+  Facebook1A,
+  Google,
+  GroupGrey,
+  IconsMail,
+  IconsPhone,
+  IconsSun,
+  ImagesGraphicsFlower,
+  ImagesLargeWaves,
+  ImagesSpiderSense,
+  Instagram,
+  Instagram1a,
+  Like,
+  LogoBlack,
+  LogoLight,
+  Mechanic1,
+  Mechanic2,
+  MechEmail,
+  MechPhone,
+  MechPin,
+  Members,
+  PersonGrey,
+  Phone1A,
+  PhoneRed2,
+  SheildBlack,
+  Speedometer,
+  StarsYellow,
+  Svg247,
+  Twitter1A,
+  Youtube1A,
+};
