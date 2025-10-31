2a35582f961c4a70a5a4b866071dd9ea2ca52b92
Benjamin Saks <saksben2@gmail.com>
2025-10-31 00:47:01 -0700

    add third review profile pic

--- assets/index.ts
+++ assets/index.ts
@@ -1,5 +1,6 @@
 import Profile4 from "./images/demo/profile-4.png";
 import Profile5 from "./images/demo/profile5.png";
+import Meet1 from "./images/demo/meet1.jpg";
 // Plumbing
 import AboutPlumbing from "./images/demo/plumbing/about.jpg";
 import About1Plumbing from "./images/demo/plumbing/about1.jpg";
@@ -16,10 +17,39 @@ import Port7Plumbing from "./images/demo/plumbing/extra7.jpg";
 import Port8Plumbing from "./images/demo/plumbing/port8.jpg";
 import ServicesPlumbing from "./images/demo/plumbing/services.jpg";
 // Roofing
+import AboutRoofing from "./images/demo/roofing/about.jpg";
+import About1Roofing from "./images/demo/roofing/about1.jpg";
+import About2Roofing from "./images/demo/roofing/about2.jpg";
+import BannerRoofing from "./images/demo/roofing/banner.jpg";
+import CTARoofing from "./images/demo/roofing/cta.jpg";
+import FAQRoofing from "./images/demo/roofing/faq.jpg";
+import HeroRoofing from "./images/demo/roofing/hero.jpg";
+import Port1Roofing from "./images/demo/roofing/port1.jpg";
+import Port2Roofing from "./images/demo/roofing/port2.jpg";
+import Port4Roofing from "./images/demo/roofing/port3.jpg";
+import Port5Roofing from "./images/demo/roofing/port4.jpg";
+import Port7Roofing from "./images/demo/roofing/port5.jpg";
+import Port8Roofing from "./images/demo/roofing/port6.jpg";
+import ServicesRoofing from "./images/demo/roofing/services.jpg";
 // Landscaping
+import AboutLandscaping from "./images/demo/landscaping/about.jpg";
+import About1Landscaping from "./images/demo/landscaping/about1.jpg";
+import About2Landscaping from "./images/demo/landscaping/about2.jpg";
+import BannerLandscaping from "./images/demo/landscaping/banner.jpg";
+import CTALandscaping from "./images/demo/landscaping/cta.jpg";
+import FAQLandscaping from "./images/demo/landscaping/faq.jpg";
+import HeroLandscaping from "./images/demo/landscaping/hero.jpg";
+import Port1Landscaping from "./images/demo/landscaping/port1.jpg";
+import Port2Landscaping from "./images/demo/landscaping/port2.jpg";
+import Port4Landscaping from "./images/demo/landscaping/port3.jpg";
+import Port5Landscaping from "./images/demo/landscaping/port4.jpg";
+import Port7Landscaping from "./images/demo/landscaping/port5.jpg";
+import Port8Landscaping from "./images/demo/landscaping/port6.jpg";
+import ServicesLandscaping from "./images/demo/landscaping/services.jpg";
 // Painting
+// Pressure Washing
 
-export { Profile4, Profile5 };
+export { Profile4, Profile5, Meet1 };
 
 export const images: Record<string, Record<string, ImageMetadata>> = {
   plumbing: {
@@ -38,21 +68,36 @@ export const images: Record<string, Record<string, ImageMetadata>> = {
     port6: Port8Plumbing,
     services: ServicesPlumbing,
   },
-  // roofing: {
-  // hero : HeroRoofing,
-  // about :
-  // whyChoose :
-  // faq :
-  // cta :
-  // banner :
-  // about1 :
-  // about2 :
-  // services :
-  // port1 :
-  // port2 :
-  // port3 :
-  // port4 :
-  // port5 :
-  // port6 :
-  // },
+  roofing: {
+    about: AboutRoofing,
+    about1: About1Roofing,
+    about2: About2Roofing,
+    banner: BannerRoofing,
+    cta: CTARoofing,
+    faq: FAQRoofing,
+    hero: HeroRoofing,
+    port1: Port1Roofing,
+    port2: Port2Roofing,
+    port3: Port4Roofing,
+    port4: Port5Roofing,
+    port5: Port7Roofing,
+    port6: Port8Roofing,
+    services: ServicesRoofing,
+  },
+  landscaping: {
+    about: AboutLandscaping,
+    about1: About1Landscaping,
+    about2: About2Landscaping,
+    banner: BannerLandscaping,
+    cta: CTALandscaping,
+    faq: FAQLandscaping,
+    hero: HeroLandscaping,
+    port1: Port1Landscaping,
+    port2: Port2Landscaping,
+    port3: Port4Landscaping,
+    port4: Port5Landscaping,
+    port5: Port7Landscaping,
+    port6: Port8Landscaping,
+    services: ServicesLandscaping,
+  },
 };
